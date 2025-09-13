a0f17b506b1c15d623bce59c2b4ed053251ed82a
lzz_zzuli <3021849674@qq.com>
2025-09-13 14:45:16 +0800

    新增员工代码

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -19,6 +20,7 @@ import springfox.documentation.spring.web.plugins.Docket;
  * 配置类，注册web层相关组件
  */
 @Configuration
+
 @Slf4j
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
